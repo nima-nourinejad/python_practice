533688300796de772c2f6408d84c2a6c078a7f72
nima-nourinejad <nima.nourinejad@gmail.com>
2024-06-11 13:48:52 +0300

    ex02 of Day 02

---
+++ Day02/ex02/load_image.py
@@ -0,0 +1,48 @@
+import numpy as np
+from PIL import Image
+
+def mode_to_channels(mode: str) -> int:
+    """
+    Accepts an image mode and returns the number of channels.
+    """
+    if mode == "1" or mode == "L":
+        return 1
+    elif mode == "RGB" or mode == "YCbCr" or mode == "LAB" or mode == "HSV":
+        return 3
+    elif mode == "RGBA" or mode == "CMYK":
+        return 4
+    else:
+        return 0
+
+def ft_load(path: str):
+    """
+     loads an image, prints its format, and its pixels
+    content in RGB format.
+    """
+    image = Image.open(path)
+    image_format = image.format
+    print(f"The format of the image is: {image_format}")
+    image_shape = [image.size[1], image.size[0], mode_to_channels(image.mode)]
+    print(f"The shape of the image is: {image_shape}")
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    all_pixels = []
+    for pixel in image.getdata():
+        all_pixels.append(list(pixel))
+    array = np.array(all_pixels)
+    return array
+
+
+def main(path: str):
+    """
+    Handles error cases and and run ft_load.
+    """
+    try:
+        ft_load(path)
+    except Exception as e:
+        print(e)
+    
+
+
+if __name__ == "__main__":
+    main()
